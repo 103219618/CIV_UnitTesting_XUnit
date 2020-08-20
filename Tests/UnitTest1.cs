ed5b57057eb027357c7a9b403a0c0286f8730986
Syed Razvi <103219618@swin.edu.au>
2020-08-20 23:50:20 +1000

    test1

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -6,10 +6,12 @@ namespace Tests
 {
     public class UnitTest1
     {
+        RecursiveCalc x = new RecursiveCalc();
+
         [Fact]
         public void Test1()
         {
-
+            Assert.Equal(720,x. FactorialRec(5,6));
         }
     }
 }
